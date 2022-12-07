44e644c1af050571869b87032925387b47f3e98c
14doan <14doan@gmail.com>
2022-12-07 07:33:44 +0000

    ere

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ const about = () => {
   return (
     <div>
       <h2>About us</h2>
-      <p>we're hear to teach you Thai, in a practical manner!</p>
+      <p>Here to teach you Thai, in a practical manner!</p>
     </div>
   );
 };
